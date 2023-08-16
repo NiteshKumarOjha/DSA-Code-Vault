9af6f3d7610826e614ddf269c52f3a4592f6a76f
Nitesh Kumar Ojha <123765583+NiteshKumarOjha@users.noreply.github.com>
2023-08-16 19:12:38 +0530

    Create Code17_DuplicateNumber.cpp

---
+++ 2. One Dimensional Array/Code17_DuplicateNumber.cpp
@@ -0,0 +1,18 @@
+/*
+  Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+  There is only one repeated number in nums, return this repeated number.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int nums[] = {1, 3, 3, 2, 2};
+
+    while (nums[0] != nums[nums[0]])
+    {
+        swap(nums[0], nums[nums[0]]);
+    }
+    cout << nums[0];
+}
